cfcbd55cf22777e44e62d7fa35d7b69e25830bbb
RamziHkiri <6257@holbertonstudents.com>
2023-05-18 10:01:06 -0700

    goes through a binary tree using post-order traversal

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ *  binary_tree_postorder - traversin a binary tree using post-order traversal
+ *  @tree: tree to be treversed
+ *  @func:pinter to the function used on the node data
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	binary_tree_inorder(tree->left, func);
+	binary_tree_inorder(tree->right, func);
+	func(tree->n);
+}
